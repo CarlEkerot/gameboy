816a9daa1ac97e4b099bccac9906b9b27c8772f2
Carl Ekerot <kalle@implode.se>
2018-04-22 23:08:55 +0200

    Add complete tests for pop and push

--- src/operations/pop.rs
+++ src/operations/pop.rs
@@ -26,11 +26,36 @@ impl Execute for Pop {
 
 #[cfg(test)]
 mod tests {
-    use test_helpers::execute_all;
+    use test_helpers::{execute_all, execute_instruction};
     use definition::Mnemonic;
+    use cpu::CPU;
+    use memory::Memory;
+    use constants::*;
 
     #[test]
     fn execute_pop() {
         execute_all(Mnemonic::POP);
+    }
+
+    #[test]
+    fn test_pop_regpair_from_stack() {
+        let pairs: [(u16, usize, usize); 4] = [
+            (0xf1, REG_A, REG_F),
+            (0xc1, REG_B, REG_C),
+            (0xd1, REG_D, REG_E),
+            (0xe1, REG_H, REG_L),
+        ];
+
+        for &(c, h, l) in pairs.iter() {
+            let mut mem = Memory::default();
+            mem.store(0xff22, 0xaa);
+            mem.store(0xff23, 0xbb);
+            let mut cpu = CPU::new(mem);
+            cpu.sp = 0xff22;
+            execute_instruction(&mut cpu, c, None);
+            assert_eq!(cpu.reg[h], 0xaa);
+            assert_eq!(cpu.reg[l], 0xbb);
+            assert_eq!(cpu.sp, 0xff24);
+        }
     }
 }
--- src/operations/push.rs
+++ src/operations/push.rs
@@ -28,11 +28,36 @@ impl Execute for Push {
 
 #[cfg(test)]
 mod tests {
-    use test_helpers::execute_all;
+    use test_helpers::{execute_all, execute_instruction};
     use definition::Mnemonic;
+    use cpu::CPU;
+    use memory::Memory;
+    use constants::*;
 
     #[test]
     fn execute_push() {
         execute_all(Mnemonic::PUSH);
+    }
+
+    #[test]
+    fn test_push_regpair_to_stack() {
+        let pairs: [(u16, usize, usize); 4] = [
+            (0xf5, REG_A, REG_F),
+            (0xc5, REG_B, REG_C),
+            (0xd5, REG_D, REG_E),
+            (0xe5, REG_H, REG_L),
+        ];
+
+        for &(c, h, l) in pairs.iter() {
+            let mut mem = Memory::default();
+            let mut cpu = CPU::new(mem);
+            cpu.sp = 0xff22;
+            cpu.reg[h] = 0xaa;
+            cpu.reg[l] = 0xbb;
+            execute_instruction(&mut cpu, c, None);
+            assert_eq!(cpu.ram.load(0xff22), 0xbb);
+            assert_eq!(cpu.ram.load(0xff21), 0xaa);
+            assert_eq!(cpu.sp, 0xff20);
+        }
     }
 }
